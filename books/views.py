723f3f05ed50f3f2d5e69458dcd1aa9290bbd706
hendisantika <hendisantika@yahoo.co.id>
2019-02-06 04:12:34 +0700

    #7 - Add Django Book as Class Base View

--- books/views.py
+++ books/views.py
@@ -1,1 +1,31 @@
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from books.models import Book
+
+
+class BookList(ListView):
+    model = Book
+
+
+class BookView(DetailView):
+    model = Book
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = ['name', 'pages']
+    success_url = reverse_lazy('book_list')
+
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['name', 'pages']
+    success_url = reverse_lazy('book_list')
+
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('book_list')
