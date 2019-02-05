b46dd2065c25e234189bbcddebdd0525dd5b6f0c
hendisantika <hendisantika@yahoo.co.id>
2019-02-06 04:09:34 +0700

    #3 - Add books

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'books'
 ]
 
 ROOT_URLCONF = 'django_crud.urls'
