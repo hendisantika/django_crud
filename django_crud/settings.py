dae9b3abee2c902fcd45193350644ee8dfe5fddb
Ryan Febriansyah <15523163@students.uii.ac.id>
2019-04-10 03:09:41 +0700

    fixed error

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -45,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'books'
 ]
 
 ROOT_URLCONF = 'django_crud.urls'
